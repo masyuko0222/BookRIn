80e0ce9e57ad94d49039299ede15e5e7012a4a80
masyuko0222 <masyuko0222@gmail.com>
2024-09-24 22:14:14 +0900

    Userモデルに新規クラスメソッドを追加
    ・Discord認証時の戻り値を利用して、Userモデルからfindかcreateを実行するクラスメソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ApplicationRecord
   validates :uid, presence: true, uniqueness: { scope: :provider }
   validates :name, presence: true
   validates :provider, presence: true
+
+  class << self
+    def find_or_create_from_discord_info(discord_info)
+      user_info = user_discord_info(discord_info)
+
+      where(user_info).first_or_create do |user|
+        user.uid = user_info[:uid]
+        user.name = user_info[:name]
+        user.provider = user_info[:provider]
+      end
+    end
+
+    private
+
+    def user_discord_info(discord_info)
+      {
+        uid: discord_info.uid,
+        name: discord_info.info.name,
+        provider: discord_info.provider
+      }
+    end
+  end
 end
