0441a28cb679506f970587f42f00ac2d6312d417
masyuko0222 <masyuko0222@gmail.com>
2024-09-24 20:42:57 +0900

    Discord認証のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/login', to: 'sessions#new'
+  get '/auth/:provider/callback', to: 'sessions#callback'
+  get '/auth/failure', to: redirect('/login')
 end
