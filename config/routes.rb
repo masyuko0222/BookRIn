d936689ccb7db3f09198d85feb9396b60fab9974
masyuko0222 <masyuko0222@gmail.com>
2024-09-26 20:47:18 +0900

    ログインページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index" # tmp path
+
   get '/login', to: 'sessions#new'
   get '/auth/:provider/callback', to: 'sessions#callback'
   get '/auth/failure', to: redirect('/login')
