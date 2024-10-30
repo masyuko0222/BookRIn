cf4bfbb89744d1445193ee72bccf8e182260cd3d
masyuko0222 <masyuko0222@gmail.com>
2024-10-30 22:47:59 +0900

    メソッド化した

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,25 +4,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @exist_user_info = OmniAuth::AuthHash.new(
-      {
-        provider: 'discord',
-        uid: '12345678',
-        info: {
-          name: 'TestMan'
-        }
-      }
-    )
-
-    @new_user_info = OmniAuth::AuthHash.new(
-      {
-        provider: 'discord',
-        uid: '987654321',
-        info: {
-          name: 'NewTestMan'
-        }
-      }
-    )
+    @exist_user_info = build_discord_auth_hash(uid: '12345678', name: 'TestMan')
+    @new_user_info = build_discord_auth_hash(uid: '987654321', name: 'NewTestMan')
   end
 
   test '.find_or_create_from_discord_info finds existing user' do
@@ -40,5 +23,16 @@ class UserTest < ActiveSupport::TestCase
       assert_equal '987654321', oauth_user.uid
       assert_equal 'NewTestMan', oauth_user.name
     end
+  end
+
+  private
+
+  # テストが多くなったらsupportsに移動すること
+  def build_discord_auth_hash(uid:, name:)
+    OmniAuth::AuthHash.new(
+      provider: 'discord',
+      uid:,
+      info: { name: }
+    )
   end
 end
