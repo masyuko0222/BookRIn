a6509b19d5a3aa85d2027d393e684c65863e5caa
masyuko0222 <masyuko0222@gmail.com>
2024-10-31 22:02:29 +0900

    テストタイトルは短くし、複雑な処理はなくす

--- test/system/reading_clubs_test.rb
+++ test/system/reading_clubs_test.rb
@@ -5,48 +5,42 @@ require 'application_system_test_case'
 class ReadingClubsTest < ApplicationSystemTestCase
   setup do
     @user = users(:user1)
-  end
 
-  test 'Opening reading_clubs ordered by updated_at desc are shown initially.' do
-    @user.participating_reading_clubs.destroy_all
-
-    visit_with_auth(reading_clubs_path, @user)
-    assert_selector 'h1', text: '輪読会一覧'
-
-    titles = page.all('ul li').map { |li| li.find('a', match: :first).text }
-
-    per_page = ReadingClub.default_per_page # １ページに15件表示される設定
-
-    start_number = 20
-    end_number = start_number - per_page + 1
-    # OpenClub 20..5の順で並ぶ
-    expected_titles = (end_number..start_number).to_a.reverse.map { |i| "OpenClub #{i}" }
-
-    assert_equal expected_titles, titles
-  end
-
-  test 'Opening reading_clubs that the current user is participating in are sorted at the top' do
-    participating_reading_clubs =
+    @participating_reading_clubs =
       [
         reading_clubs(:reading_club1),
         reading_clubs(:reading_club5),
         reading_clubs(:reading_club20)
       ]
 
-    participating_reading_clubs.each do |reading_club|
+    @participating_reading_clubs.each do |reading_club|
       @user.participants.create!(reading_club_id: reading_club.id)
     end
-
-    visit_with_auth(reading_clubs_path, @user)
-
-    titles = page.all('ul li').map { |li| li.find('a', match: :first).text }
-    # 最近参加した輪読会から順に出力される
-    expected_top_titles = ['OpenClub 20', 'OpenClub 5', 'OpenClub 1']
-
-    assert_equal expected_top_titles, titles.first(3)
   end
 
-  test 'finished reading_clubs does not have participant links' do
+  test 'Opening reading_clubs are ordered by updated_at desc.' do
+    @user.participating_reading_clubs.destroy_all
+
+    visit_with_auth(reading_clubs_path, @user)
+    assert_selector 'h1', text: '輪読会一覧'
+
+    club_titles = page.all('ul li').map { |li| li.find('a', match: :first).text }
+    expected_titles = (6..20).to_a.reverse.map { |i| "OpenClub #{i}" } # per_page 15
+
+    assert_equal expected_titles, club_titles
+  end
+
+  test 'Participating opening reading_clubs are sorted at the top' do
+    visit_with_auth(reading_clubs_path, @user)
+    assert_selector 'h1', text: '輪読会一覧'
+
+    club_titles = page.all('ul li').map { |li| li.find('a', match: :first).text }
+    expected_top_titles = ['OpenClub 20', 'OpenClub 5', 'OpenClub 1'] # 参加日降順
+
+    assert_equal expected_top_titles, club_titles.first(3)
+  end
+
+  test 'Finished reading_clubs does not have participant links' do
     visit_with_auth(reading_clubs_path, @user)
     fill_in '輪読会のタイトルで検索', with: ''
     choose '終了'
