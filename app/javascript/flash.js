7269b3c010fbe5d6011c4ce69f19bd416a1aa0b4
masyuko0222 <masyuko0222@gmail.com>
2024-11-12 23:36:40 +0900

    🚨 lint, prettier

--- app/javascript/flash.js
+++ app/javascript/flash.js
@@ -1,14 +1,14 @@
-const closeFlash= () => {
-  const flash = document.getElementById('flash');
+const closeFlash = () => {
+	const flash = document.getElementById('flash');
 
-  window.closeFlash= () => {
-    if (flash) {
-      flash.style.transition = 'opacity 0.5s ease';
-      flash.style.opacity = '0';
-      setTimeout(() => flash.remove(), 500);
-    }
-  };
-}
+	window.closeFlash = () => {
+		if (flash) {
+			flash.style.transition = 'opacity 0.5s ease';
+			flash.style.opacity = '0';
+			setTimeout(() => flash.remove(), 500);
+		}
+	};
+};
 
 document.addEventListener('turbo:load', closeFlash);
 document.addEventListener('turbo:render', closeFlash);
