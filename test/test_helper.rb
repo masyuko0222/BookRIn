55224b30d06132b900626acef6d1f97b4d5153d1
masyuko0222 <masyuko0222@gmail.com>
2024-11-19 20:56:59 +0900

    システムテストでVCRが利用されないようにした

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,6 +17,7 @@ module ActiveSupport
   end
 
   VCR.configure do |config|
+    config.ignore_hosts '127.0.0.1', 'localhost'
     config.cassette_library_dir = 'fixtures/vcr_cassettes'
     config.hook_into :webmock
 
