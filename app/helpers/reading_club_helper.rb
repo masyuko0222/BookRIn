44798df0ccc8bbacd1bf89e006eb1eafafeb6d2d
masyuko0222 <masyuko0222@gmail.com>
2024-11-24 22:20:01 +0900

    radio_buttonをDRYにした

---
+++ app/helpers/reading_club_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module ReadingClubHelper
+  def radio_button_with_label(form, method, tag_value, label_text, id:, checked: false)
+    content_tag :label, class: 'inline-flex items-center space-x-1' do
+      concat(form.radio_button(method, tag_value, id:, checked:))
+      concat(content_tag(:span, label_text, class: 'text-sm text-gray-700'))
+    end
+  end
+end
