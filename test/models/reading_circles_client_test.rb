571ae4f9141867f7f5f2fb8944c2d6276d73a4c4
masyuko0222 <masyuko0222@gmail.com>
2024-11-24 21:24:27 +0900

    テスト修正
    ・.saveの出来ることごとにテストを分けた
    ・setupやfixutesをわざわざ見る手間をなくすため、テスト内でデータを作るようにした

--- test/models/reading_circles_client_test.rb
+++ test/models/reading_circles_client_test.rb
@@ -3,20 +3,6 @@
 require 'test_helper'
 
 class ReadingCirclesClientTest < ActiveSupport::TestCase
-  setup do
-    reading_clubs(:to_update_reading_club)
-    reading_clubs(:to_destroy_reading_club)
-    reading_clubs(:no_change_reading_club)
-
-    @data = {
-      'reading_circles' => [
-        { 'id' => 1000, 'title' => 'UpdatedClub', 'finished' => true, 'updated_at' => Time.zone.parse('2024-01-01') },
-        { 'id' => 3000, 'title' => 'NoChangeClub', 'finished' => false, 'updated_at' => Time.zone.parse('2000-01-01') },
-        { 'id' => 4000, 'title' => 'NewClub', 'finished' => false, 'updated_at' => Time.zone.parse('2030-01-01') }
-      ]
-    }
-  end
-
   test '.fetch' do
     VCR.use_cassette('fetch_reading_circles') do
       result = ReadingCirclesClient.fetch
@@ -26,36 +12,71 @@ class ReadingCirclesClientTest < ActiveSupport::TestCase
     end
   end
 
-  test '.save' do
-    to_update_club = ReadingClub.create!(
+  test '.save as update' do
+    ReadingClub.create!(
+      id: 10000,
       title: 'ToUpdateClub', finished: false,
       template: nil, read_me: nil,
       updated_at: Time.zone.parse('2000-01-01')
       )
 
-    to_destroy_club = ReadingClub.create!(
+    api_data = {
+      'reading_circles' => [
+        {
+          'id' => 10000,
+          'title' => 'UpdatedClub',
+          'finished' => true,
+          'updated_at' => Time.zone.parse('2024-01-01')
+        }
+      ]
+    }
+
+    latest_clubs = api_data['reading_circles']
+    ReadingCirclesClient.save(latest_clubs)
+
+    updated_club = ReadingClub.find(10000)
+    assert_equal 'UpdatedClub', updated_club.title
+    assert updated_club.finished
+    assert_equal Time.zone.parse('2024-01-01'), updated_club.updated_at
+  end
+
+  test '.save as create' do
+    api_data = {
+      'reading_circles' => [
+        {
+          'id' => 20000,
+          'title' => 'NewClub',
+          'finished' => false,
+          'updated_at' => Time.zone.parse('2024-01-01')
+        }
+      ]
+    }
+
+    latest_clubs = api_data['reading_circles']
+    ReadingCirclesClient.save(latest_clubs)
+
+    new_club = ReadingClub.find_by(title: 'NewClub')
+    assert_not new_club.finished
+    assert_equal Time.zone.parse('2024-01-01'), new_club.updated_at
+  end
+
+  test '.save as destroy' do
+    ReadingClub.create!(
+      id: 30000,
       title: 'ToDestroyClub', finished: true,
       template: nil, read_me: nil,
       updated_at: Time.zone.parse('2000-01-01')
       )
 
-    no_change_club = ReadingClub.create!(
-      title: 'NoChangeClub', finished: false,
-      template: nil, read_me: nil,
-      updated_at: Time.zone.parse('2000-01-01')
-      )
+    api_data = {
+      'reading_circles' => []
+    }
 
-    latest_clubs = @data['reading_circles']
+    latest_clubs = api_data['reading_circles']
     ReadingCirclesClient.save(latest_clubs)
 
-    updated_club = ReadingClub.find_by(title: 'UpdatedClub')
-    assert updated_club.finished
-    assert_equal Time.zone.parse('2024-01-01'), updated_club.updated_at
-
-    assert_nil ReadingClub.find_by(title: 'ToDestroyClub')
-
-    new_club = ReadingClub.find_by(title: 'NewClub')
-    assert_not new_club.finished
-    assert_equal Time.zone.parse('2030-01-01'), new_club.updated_at
+    assert_raises(ActiveRecord::RecordNotFound) do
+      ReadingClub.find(30000)
+    end
   end
 end
