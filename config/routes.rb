5410b116c6d1b6699b79e6f57a594750cbaad092
masyuko0222 <masyuko0222@gmail.com>
2024-12-16 20:38:03 +0900

    noteのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     resource :overview, only: [:show], controller: 'reading_clubs/overview'
     resources :participants, only: [:create, :destroy], shallow: true, controller: 'reading_clubs/participants'
     resource :read_me, only: [:edit, :update], controller: 'reading_clubs/read_me'
+    resources :notes, only: [:new, :edit, :update, :destroy], shallow: true, controller: 'notes'
   end
 end
