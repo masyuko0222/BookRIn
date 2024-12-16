695ec2073d67f5c160677f9a36da98c50b54991d
masyuko0222 <masyuko0222@gmail.com>
2024-12-16 21:51:24 +0900

    ノート一覧でnotes#new, showに飛べるようにした

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class NotesController < ApplicationController
+  def new; end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+end
+
