264638830d926818450448df59373ac4a0aff098
masyuko0222 <masyuko0222@gmail.com>
2024-12-19 22:54:08 +0900

    MarkdowonParserHelperのテスト

---
+++ test/helpers/markdown_parser_helper_test.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class MarkdownParserHelperTest < ActiveSupport::TestCase
+  include MarkdownParserHelper
+  include ActionView::Helpers::SanitizeHelper
+
+  test '.to_html convert to HTML from Markdown' do
+    md_text = <<~MARKDOWN
+      # プログラミング基礎読書会
+
+      **この読書会では、プログラミングの基礎を学びます。**
+
+      - 開催日時: 毎週土曜日 午後2時〜午後4時
+
+      ## 学習項目
+      - 変数とデータ型
+      - 条件分岐とループ
+      - 基本的なアルゴリズム
+
+      を中心に学習していきます。
+      ぜひ一緒に学びましょう！
+
+      [リンク](https://example.com/)
+    MARKDOWN
+
+
+    expected_html = <<~HTML
+      <h1>プログラミング基礎読書会</h1>
+
+      <p><strong>この読書会では、プログラミングの基礎を学びます。</strong></p>
+
+      <ul>
+      <li>開催日時: 毎週土曜日 午後2時〜午後4時</li>
+      </ul>
+
+      <h2>学習項目</h2>
+
+      <ul>
+      <li>変数とデータ型</li>
+      <li>条件分岐とループ</li>
+      <li>基本的なアルゴリズム</li>
+      </ul>
+
+      <p>を中心に学習していきます。
+      ぜひ一緒に学びましょう！</p>
+
+      <p><a href="https://example.com/">リンク</a></p>
+    HTML
+
+    assert_equal expected_html, to_html(md_text)
+  end
+end
