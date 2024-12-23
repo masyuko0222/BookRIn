f66fe2000988eaaef922b32acbbbd33661da37ce
masyuko0222 <masyuko0222@gmail.com>
2024-12-23 21:01:39 +0900

    リファクタリング前に無闇なpartialやhelperを元に戻した

--- app/helpers/reading_club_helper.rb
+++ app/helpers/reading_club_helper.rb
@@ -1,17 +1,6 @@
 # frozen_string_literal: true
 
 module ReadingClubHelper
-  def radio_button_with_label(form, method, tag_value, options = {})
-    id = options[:id]
-    checked = options[:checked]
-    label_text = options[:label_text]
-
-    content_tag :label, class: 'inline-flex items-center space-x-1' do
-      concat(form.radio_button(method, tag_value, id:, checked:))
-      concat(content_tag(:span, label_text, class: 'text-sm text-gray-700'))
-    end
-  end
-
   def read_me_to_html(md_text)
     return unless md_text
 
