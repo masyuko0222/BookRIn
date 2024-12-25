25a9c9cb07aa6b4993ea62d6ab81cebe83b2da5c
masyuko0222 <masyuko0222@gmail.com>
2024-12-25 22:15:11 +0900

    sleepの代わりの行追加

--- test/system/reading_clubs_test.rb
+++ test/system/reading_clubs_test.rb
@@ -29,6 +29,7 @@ class ReadingClubsTest < ApplicationSystemTestCase
     within("[data-test-id='participating-status']") { choose('開催中') }
     within("[data-test-id='finished-status']") { choose('すべて') }
 
+    assert_text 'OpenClub 9' # sleepの代わり
     hit_titles = page.all('ul li').map { |li| li.find('a', match: :first).text }
 
     assert_equal hit_titles, ['OpenClub 9']
