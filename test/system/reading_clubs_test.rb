9706ec6358debc4bb66b3978828813a8eba5c8ac
masyuko0222 <masyuko0222@gmail.com>
2024-12-25 22:58:30 +0900

    参加ボタンの確認はsetupで参加している輪読会を利用するようにした
    ・ラジオボタンによる検索も一緒に確認

--- test/system/reading_clubs_test.rb
+++ test/system/reading_clubs_test.rb
@@ -10,17 +10,25 @@ class ReadingClubsTest < ApplicationSystemTestCase
 
   test 'Click participant button' do
     visit_with_auth(reading_clubs_path, @user)
-    assert_current_path reading_clubs_path
+    within("[data-test-id='participating-status']") { choose('開催中') }
+    within("[data-test-id='finished-status']") { choose('参加中のみ') }
+    assert_text 'OpenClub 1'
+
+    within(find('li', text: 'OpenClub 1')) do
+      click_link '参加取消'
+    end
+
+    within("[data-test-id='participating-status']") { choose('開催中') }
+    within("[data-test-id='finished-status']") { choose('参加中のみ') }
+    assert_text '参加中の輪読会はありません'
+
+    fill_in '輪読会のタイトルで検索', with: 'OpenClub 1'
     within("[data-test-id='participating-status']") { choose('開催中') }
     within("[data-test-id='finished-status']") { choose('すべて') }
 
-    within(find('li', text: 'OpenClub 20')) do
-      click_link '参加'
-      assert_selector 'a', text: '参加取消'
-    end
+    assert_text 'OpenClub 1' # sleepの代わり
 
-    within(find('li', text: 'OpenClub 20')) do
-      click_link '参加取消'
+    within(find('li', text: /^OpenClub 1$/)) do
       assert_selector 'a', text: '参加'
     end
   end
