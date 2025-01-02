958dd2230440cc4a8224f30a0077857593df9be1
masyuko0222 <masyuko0222@gmail.com>
2025-01-02 22:39:49 +0900

    参加/参加取消リンクを輪読会の詳細画面に移動して、のロジックもhelperにした
    ・一覧でリンクを押した時に、ユーザーが何が起こるのかイメージがつきづらい
      - 参加=登録的なイメージにもなるので、押すのが怖いかも

--- app/helpers/reading_club_helper.rb
+++ app/helpers/reading_club_helper.rb
@@ -9,4 +9,12 @@ module ReadingClubHelper
     markdown = Redcarpet::Markdown.new(renderer)
     markdown.render(md_text)
   end
+
+  def participant_link(user, reading_club)
+    if participant = user.participants.find_by(reading_club:)
+      link_to '参加取消', participant_path(participant), data: { turbo_method: :delete }
+    else
+      link_to '参加', reading_club_participants_path(reading_club), data: { turbo_method: :post }
+    end
+  end
 end
--- test/helpers/reading_club_helper_test.rb
+++ test/helpers/reading_club_helper_test.rb
@@ -4,5 +4,27 @@ require 'test_helper'
 
 class ReadingClubHelperTest < ActiveSupport::TestCase
   include ReadingClubHelper
-  # 後でテスト追加する
+  include Rails.application.routes.url_helpers
+  include ActionView::Helpers::UrlHelper
+
+  test '.participant_link as 参加リンク' do
+    user = users(:user1)
+    reading_club = reading_clubs(:reading_club1)
+    link = participant_link(user, reading_club)
+
+    assert_includes link, '参加'
+    assert_includes link, reading_club_participants_path(reading_club)
+    assert_includes link, "data-turbo-method=\"post\""
+  end
+
+  test '.participant_link as 参加取消リンク' do
+    user = users(:user1)
+    reading_club = reading_clubs(:reading_club1)
+    participant = Participant.create!(user:, reading_club:)
+    link = participant_link(user, reading_club)
+
+    assert_includes link, '参加取消'
+    assert_includes link, participant_path(participant)
+    assert_includes link, "data-turbo-method=\"delete\""
+  end
 end
