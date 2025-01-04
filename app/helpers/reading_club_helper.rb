e820ac3f0b02e6a7c4cbe59a89a40f67a6fa78fc
masyuko0222 <masyuko0222@gmail.com>
2025-01-04 19:05:43 +0900

    参加ボタンのデザインのデザイン

--- app/helpers/reading_club_helper.rb
+++ app/helpers/reading_club_helper.rb
@@ -12,9 +12,17 @@ module ReadingClubHelper
 
   def participant_link(user, reading_club)
     if participant = user.participants.find_by(reading_club:)
-      link_to '参加取消', participant_path(participant), data: { turbo_method: :delete }
+      link_to '参加取消', participant_path(participant), data: { turbo_method: :delete },
+      class: 'items-center px-3 py-1
+              text-gray-600 bg-gray-100
+              border border-gray-300 rounded-lg
+              cursor-pointe hover:bg-gray-200'
     else
-      link_to '参加', reading_club_participants_path(reading_club), data: { turbo_method: :post }
+      link_to '輪読会に参加する', reading_club_participants_path(reading_club), data: { turbo_method: :post },
+      class: 'items-center px-3 py-1
+              text-white bg-main-color
+              border border-main-color rounded-lg
+              cursor-pointer hover:bg-blue-700'
     end
   end
 end
