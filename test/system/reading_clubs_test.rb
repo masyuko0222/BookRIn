1c3ba39b28e4e8e9fcc90134858589b0275e055e
masyuko0222 <masyuko0222@gmail.com>
2025-01-05 14:52:15 +0900

    テストを書き直し

--- test/system/reading_clubs_test.rb
+++ test/system/reading_clubs_test.rb
@@ -8,30 +8,6 @@ class ReadingClubsTest < ApplicationSystemTestCase
     @user.participants.create!(reading_club: reading_clubs(:reading_club1))
   end
 
-  test 'Click participant button' do
-    visit_with_auth(reading_clubs_path, @user)
-    within("[data-test-id='participating-status']") { choose('開催中') }
-    within("[data-test-id='finished-status']") { choose('参加中のみ') }
-    assert_text 'OpenClub 1'
-
-    within(find('li', text: 'OpenClub 1')) do
-      click_link '参加取消'
-    end
-
-    within("[data-test-id='participating-status']") { choose('開催中') }
-    within("[data-test-id='finished-status']") { choose('参加中のみ') }
-    assert_text '参加中の輪読会はありません'
-
-    fill_in '輪読会のタイトルで検索', with: 'OpenClub 1'
-    within("[data-test-id='participating-status']") { choose('開催中') }
-    within("[data-test-id='finished-status']") { choose('すべて') }
-
-    assert_text 'OpenClub 1' # sleepの代わり
-
-    within(find('li', text: /^OpenClub 1$/)) do
-      assert_selector 'a', text: '参加'
-    end
-  end
 
   test 'Search title' do
     visit_with_auth(reading_clubs_path, @user)
