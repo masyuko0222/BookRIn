80022f625370dcc10aa4675d9494e0f375a4cbe2
masyuko0222 <masyuko0222@gmail.com>
2025-01-08 21:03:26 +0900

    シンプルなnote編集ページを作成

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 class NotesController < ApplicationController
-  before_action :set_note, only: %i[edit destroy]
+  before_action :set_note, only: %i[edit update destroy]
 
   def new; end
 
   def edit; end
 
-  def update; end
+  def update
+    if @note.update(note_params)
+      flash.now.notice = 'ノートを更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def destroy
     @note.destroy
@@ -18,5 +24,9 @@ class NotesController < ApplicationController
 
   def set_note
     @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:held_on, :title, :content)
   end
 end
