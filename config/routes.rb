537822064f2ae443420a4064ca29e659690311ee
masyuko0222 <masyuko0222@gmail.com>
2025-01-29 22:55:20 +0900

    ノート用のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
     get 'overview', on: :member
     resources :participants, only: [:create, :destroy], shallow: true, controller: 'reading_clubs/participants'
     resource :read_me, only: [:edit, :update], controller: 'reading_clubs/overview/read_me'
-    resources :notes, shallow: true, controller: 'notes'
+    resources :notes, only: [:new, :edit, :create, :update, :destroy], shallow: true, controller: 'notes'
   end
 end
