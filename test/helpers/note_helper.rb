7133796f97fad4077a1b832fe246011346bcc1dd
masyuko0222 <masyuko0222@gmail.com>
2025-01-30 21:25:59 +0900

    viewの長いロジックはhelperに移動した

--- test/helpers/note_helper.rb
+++ test/helpers/note_helper.rb
@@ -3,6 +3,7 @@
 require 'test_helper'
 
 class NoteHelperTest < ActiveSupport::TestCase
+  include Rails.application.routes.url_helpers
   include NoteHelper
 
   test '.title_with_held_on(note)' do
@@ -14,5 +15,21 @@ class NoteHelperTest < ActiveSupport::TestCase
     expected_title = '1月1日 新年ノート'
 
     assert_equal expected_title, title_with_held_on(note)
+  end
+
+  test '.submit_button_text' do
+    new_note = Note.new
+    assert_equal 'ノートを新規作成', submit_button_text(new_note)
+
+    exist_note = notes(:note1)
+    assert_equal 'ノートを更新', submit_button_text(exist_note)
+  end
+
+  test 'note_form_url' do
+    new_note = Note.new(reading_club: reading_clubs(:reading_club1))
+    assert_equal reading_club_notes_path(new_note.reading_club), note_form_url(new_note)
+
+    exist_note = notes(:note1)
+    assert_equal note_path(exist_note), note_form_url(exist_note)
   end
 end
