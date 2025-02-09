848a01b236560ad0680686762668b4a512db0ab9
masyuko0222 <masyuko0222@gmail.com>
2025-02-09 22:51:39 +0900

    proptypesが走らないようにした

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,32 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import eslintConfigPrettier from "eslint-config-prettier"
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   {
     settings: {
       react: {
-        version: 'detect',
-      }
-    }
+        version: "detect",
+      },
+    },
   },
   {
-    languageOptions:
-    {
+    languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
-      }
-    }
+      },
+    },
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
+  {
+    files: ["**/*.jsx"],
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
 ];
