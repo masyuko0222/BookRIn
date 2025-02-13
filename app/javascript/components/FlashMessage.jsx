7a13ca5714fd4fce3fdcdaac71768b2fa4d00d77
masyuko0222 <masyuko0222@gmail.com>
2025-02-13 22:44:11 +0900

    prettier

--- app/javascript/components/FlashMessage.jsx
+++ app/javascript/components/FlashMessage.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export const FlashMessage = ({ message, onCloseFlash }) => (
 	<div className='fixed top-4 right-4 z-50 flex items-center px-4 py-3 rounded-lg text-white bg-green-500 bg-opacity-80'>
 		<p className='font-semibold mr-2'>{message}</p>
-		<button type='button' onClick={onCloseFlash} className='text-lg font-bold cursor-pointer'>×</button>
+		<button type='button' onClick={onCloseFlash} className='text-lg font-bold cursor-pointer'>
+			×
+		</button>
 	</div>
 );
