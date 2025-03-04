70488d242c792146d0e8be9c0f531e0b0e443fb9
masyuko0222 <masyuko0222@gmail.com>
2025-03-04 21:27:54 +0900

    共同編集機能のあるEditorと無いEditorを異なるコンポーネントとして作成

--- app/javascript/components/NoteEditor.jsx
+++ app/javascript/components/NoteEditor.jsx
@@ -1,53 +1,29 @@
-import React, { useEffect, useRef } from 'react';
-import { EditorContent } from '@tiptap/react';
-import { useEditor } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import Collaboration from '@tiptap/extension-collaboration';
+import React, { useEffect, useState } from 'react';
 import { WebsocketProvider } from 'y-websocket';
+import { NewNoteEditor } from './NewNoteEditor';
+import { CollabNoteEditor } from './CollabNoteEditor';
 import * as Y from 'yjs';
 
 const yDoc = new Y.Doc();
 
 export const NoteEditor = ({ setEditor, isNew, noteId, content, currentTemplate, changeContent }) => {
-  const wsProvider = useRef(null);
+  const [wsProvider, setWsProvider] = useState(null);
 
   useEffect(() => {
-    setEditor(editor);
     if (isNew) return;
-    wsProvider.current = new WebsocketProvider('ws://localhost:5678', noteId, yDoc);
-    wsProvider.current?.connect();
 
-    wsProvider.current?.on('sync', (isSynced) => {
-      if (isSynced) {
-        if (!yDoc.getMap('config').get('initialContentLoaded') && editor) {
-          yDoc.getMap('config').set('initialContentLoaded', true);
-          editor.commands.setContent(content);
-          document.getElementById('note-editor-hidden').value = content;
-        }
-      }
-    });
+    const provider = new WebsocketProvider('ws://localhost:5678', noteId, yDoc);
+    setWsProvider(provider);
 
     return () => {
-      wsProvider.current?.destroy();
-      yDoc?.destroy();
+      wsProvider?.destroy();
+      yDoc.destroy();
     };
   }, []);
 
-  const editor = useEditor({
-    extensions: [StarterKit, ...(isNew ? [] : [Collaboration.configure({ document: yDoc })])],
-    editorProps: {
-      attributes: {
-        class: 'mr-2 border border-gray-300 p-4 rounded focus:ring-blue-500',
-        style: 'height: 70vh; overflow: auto;',
-      },
-    },
-    onCreate({ editor }) {
-      isNew && changeContent(editor, currentTemplate);
-    },
-    onUpdate({ editor }) {
-      document.getElementById('note-editor-hidden').value = editor.getHTML();
-    },
-  });
-
-  return <EditorContent editor={editor} />;
+  return isNew ? (
+    <NewNoteEditor setEditor={setEditor} currentTemplate={currentTemplate} changeContent={changeContent} />
+  ) : (
+    <CollabNoteEditor yDoc={yDoc} setEditor={setEditor} wsProvider={wsProvider} content={content} />
+  );
 };
