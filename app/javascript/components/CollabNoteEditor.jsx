81b8ebf8193205b099cdf23f5390d3e105f9e1c6
masyuko0222 <masyuko0222@gmail.com>
2025-03-25 20:54:25 +0900

    ノートのxマージンを広くした。すべてのマージンの要素を揃えた

--- app/javascript/components/CollabNoteEditor.jsx
+++ app/javascript/components/CollabNoteEditor.jsx
@@ -18,7 +18,7 @@ export const CollabNoteEditor = ({ wsProvider, yDoc, setEditor, content }) => {
     extensions: [StarterKit, Collaboration.configure({ document: yDoc })],
     editorProps: {
       attributes: {
-        class: 'mr-2 border border-gray-300 p-4 rounded focus:ring-blue-500',
+        class: 'border border-gray-300 p-4 rounded focus:ring-blue-500',
         style: 'height: 70vh; overflow: auto;',
       },
     },
