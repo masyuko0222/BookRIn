fc4d60aa1ae0c28a2cf7aa443cc668cd92fc64ea
masyuko0222 <masyuko0222@gmail.com>
2025-03-27 23:03:11 +0900

    検索ワードと本文内の文字列がマッチしたら、マッチした文字列を含む前後何文字かの本文を返すようにした

--- app/helpers/note_helper.rb
+++ app/helpers/note_helper.rb
@@ -14,4 +14,18 @@ module NoteHelper
     # そのためform_withが自動でurlを判別できないので、helperでロジックを定義しておく
     note.new_record? ? reading_club_notes_path(note.reading_club) : note_path(note)
   end
+
+  def highlight_query_word(text, query_word, select_counts: 100)
+    return if text.nil?
+    plain_text = strip_tags(text.gsub(/<\/(p|li|h[1-6]|br|div|ul|ol)>/, '</\1> '))
+
+    match = plain_text.match(query_word)
+    return if match.nil?
+
+    highlight_start_index = [(match.begin(0) - select_counts), 0].max
+    highlight_end_index = [(match.end(0) + select_counts), plain_text.length].min
+
+    highlight = plain_text[highlight_start_index..highlight_end_index]
+    highlight + '...'
+  end
 end
