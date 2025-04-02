cc3af1c19fb136885823f687a55be0ec05b42b0c
masyuko0222 <masyuko0222@gmail.com>
2025-04-02 21:22:38 +0900

    scope moduleを利用して、controllerオプションでreading_clubsを明示しなくて良いようにした

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,14 @@ Rails.application.routes.draw do
   resource :privacy, only: [:show], controller: 'privacy'
 
   resources :reading_clubs, only: [:index] do
-    resources :participants, only: [:create, :destroy], shallow: true, controller: 'reading_clubs/participants'
-    resource :overview, only: [:show], controller: 'reading_clubs/overview'
-    resource :read_me, only: [:edit, :update], controller: 'reading_clubs/overview/read_me'
-    resource :template, only: [:update], controller: 'reading_clubs/template'
-    resources :notes, only: [:new, :edit, :create, :update], shallow: true, controller: 'notes'
+    scope module: :reading_clubs do
+      resource :overview, only: [:show], controller: 'overview'
+      resource :template, only: [:update], controller: 'template'
+      resource :read_me, only: [:edit, :update], controller: 'overview/read_me'
+
+      resources :participants, only: [:create, :destroy], shallow: true
+    end
+
+    resources :notes, only: [:new, :edit, :create, :update], shallow: true
   end
 end
