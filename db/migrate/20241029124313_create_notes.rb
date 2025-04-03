dd20722795d6ce6cb8b758bdb0db529ed683a886
masyuko0222 <masyuko0222@gmail.com>
2025-04-03 22:26:23 +0900

    カラムに制約をつけた
    ・noteのtitleとcontentにnot null
    ・reading_clubのtemplateとread_meにnot null
    ・reading_clubのbooleanであるfinishedにdefault値
    ・participantsのreading_club_idとuser_idにuqni index

--- db/migrate/20241029124313_create_notes.rb
+++ db/migrate/20241029124313_create_notes.rb
@@ -2,8 +2,8 @@ class CreateNotes < ActiveRecord::Migration[7.1]
   def change
     create_table :notes do |t|
       t.date :held_on, null: false
-      t.string :title
-      t.text :content
+      t.string :title, null: false
+      t.text :content, null: false
       t.references :reading_club, null: false, foreign_key: true
 
       t.timestamps
