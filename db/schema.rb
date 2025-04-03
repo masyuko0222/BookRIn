56b265f7d5d466a1dec15ba84409aeb4293c6b78
masyuko0222 <masyuko0222@gmail.com>
2025-04-03 22:08:13 +0900

    更新しやすくするため、templateとread_meのdefaultはmodelのattributeで定義
    ・本番環境はまだデータがないので、migrationも削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_26_125522) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_124313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_26_125522) do
   create_table "reading_clubs", force: :cascade do |t|
     t.string "title", null: false
     t.boolean "finished", null: false
-    t.text "template", default: "### これはテンプレート例です\n  - 「作成」ボタンを押して、新しいノートを作成しましょう！\n  - 作成したノートは共同編集ができます。輪読会で学んだことをみんなで記録しましょう😀\n\n### 今日読んだ範囲\n   - 「第〇章 p.xx」～「第〇章 p.yy」\n\n### 学んだこと・分かったこと\n  - Aさん\n    - 〇〇というメソッドは、配列を△△したいときに利用する\n  - Bさん\n"
-    t.text "read_me", default: "# READ ME\n**輪読会のルールや読んで欲しいことを書きましょう！**\n\n## 開催日時\n- 毎週月曜日 20:00~\n- 毎週水曜日 20:00~\n- 祝日はお休みです\n\n## Discordリンク\n[リンク](https://example.com/)\n"
+    t.text "template"
+    t.text "read_me"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
