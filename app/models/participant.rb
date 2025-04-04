9391b23865e79ff1cd9340547fd3125c79a29e35
masyuko0222 <masyuko0222@gmail.com>
2025-04-04 19:16:17 +0900

    モデル側もschemaに合わせてバリデーション

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Participant < ApplicationRecord
+  validates :user_id, uniqueness: { scope: :reading_club_id }
+
   belongs_to :user
   belongs_to :reading_club
 end
