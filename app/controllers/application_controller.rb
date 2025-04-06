b3298f3e11be2710c5041f9d7bd2e909fb4cd90f
masyuko0222 <masyuko0222@gmail.com>
2025-04-06 20:06:11 +0900

    ログインに関する処理は、concernsのAuthentication moduleにまとめた
    ・current_userはView以外にもControllerなどで使っているため、concernsに書いたほうが良いため
    ・また、Viewではcurrent_userくらいしか使っていないので、他のメソッドをHelperに書いておく必要がないため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include SessionHelper
-
   include Authentication
+  helper_method :current_user
   before_action :require_user_login
 end
