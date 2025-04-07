df3c4ff352d61025f042f38ae1219d2ce579a8f3
masyuko0222 <masyuko0222@gmail.com>
2025-04-07 21:27:04 +0900

    rubocopから除外していたreading_club_helperを、除外しないように修正

--- app/helpers/reading_club_helper.rb
+++ app/helpers/reading_club_helper.rb
@@ -11,13 +11,15 @@ module ReadingClubHelper
   end
 
   def participant_link(user, reading_club)
-    if participant = user.participants.find_by(reading_club:)
-      link_to '参加取消', participant_path(participant), data: { turbo_method: :delete },
-      class: "underline cursor-pointer text-gray-600"
+    if (participant = user.participants.find_by(reading_club:))
+      link_to '参加取消', participant_path(participant),
+              data: { turbo_method: :delete }, class: 'underline cursor-pointer text-gray-600'
     else
-      link_to '＋ 輪読会に参加する', reading_club_participants_path(reading_club), data: { turbo_method: :post },
-      class: "items-center px-4 py-2 cursor-pointer border border-link-border rounded-lg
-              text-white bg-main-color hover:bg-blue-700"
+      link_to '＋ 輪読会に参加する',
+              reading_club_participants_path(reading_club),
+              data: { turbo_method: :post },
+              class: 'items-center px-4 py-2 cursor-pointer border
+                border-link-border rounded-lg text-white bg-main-color hover:bg-blue-700'
     end
   end
 end
