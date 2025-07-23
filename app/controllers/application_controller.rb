fdbe28342e5b2220b8a8aab2fe59771f014d1168
masyuko0222 <masyuko0222@gmail.com>
2025-07-23 20:15:45 +0900

    store_locationをAuthentication Moduleに移動
    ・他のセッション関係のメソッドは、Authentication Moduleにまとめているため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
   helper_method :current_user
-  before_action :store_location
   before_action :require_user_login
-
-  def store_location
-    return unless request.get?
-    return if request.xhr?
-    return if request.path.in?([login_path, '/auth/discord/callback', '/auth/failure'])
-
-    session[:previous_url] = request.fullpath
-  end
+  before_action :store_location
 end
